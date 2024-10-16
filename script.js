6ba7cf77598711fdefa835a33b42cc9b7a41c8ca
Viacheslav Shatailo <sdgamer2588@gmail.com>
2024-10-16 12:19:07 +0300

    modal scroll fix

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const toggleBtn = document.getElementById('htb')
 const modal = document.getElementById('modal')
+const body = document.body
+const html = document.documentElement
 
 toggleBtn.addEventListener('click', () => {
 	modal.classList.toggle('modal--inactive')
 	toggleBtn.classList.toggle('header__toggle-btn--hamburger')
 	toggleBtn.classList.toggle('header__toggle-btn--x')
-	console.log('click!')
+
+	// Check if modal is active or inactive and update scrolling
+	if (modal.classList.contains('modal--inactive')) {
+		body.classList.remove('no-scroll')
+		html.classList.remove('no-scroll')
+	} else {
+		body.classList.add('no-scroll')
+		html.classList.add('no-scroll')
+	}
+
+	console.log('Modal toggled and scroll behavior updated')
 })
